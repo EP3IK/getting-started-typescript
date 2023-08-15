f924303ec26e472ed9a8222e10c62312c3aa2dde
김진배/FE서비스개발팀/CL <jinbae.kim@nhn.com>
2023-08-16 05:07:39 +0900

    feat: chapter 04 second practice

--- chapter04/learn-typescript/quiz/1_todo/src/index.ts
+++ chapter04/learn-typescript/quiz/1_todo/src/index.ts
@@ -4,7 +4,7 @@ type TodoItem = {
   done: boolean;
 };
 
-let todoItems;
+let todoItems: TodoItem[];
 
 // api
 function fetchTodoItems(): TodoItem[] {
@@ -22,15 +22,15 @@ function fetchTodos(): TodoItem[] {
   return todos;
 }
 
-function addTodo(todo): void {
+function addTodo(todo: TodoItem): void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index): void {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo): void {
+function completeTodo(index: number, todo: TodoItem): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
@@ -45,12 +45,14 @@ function showCompleted(): TodoItem[] {
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  addTodo({ id: 4, title: '아무', done: false });
+  addTodo({ id: 5, title: '거나', done: false });
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
